4a2b9d469a8b365d6951a08fb0bc70e3b0302a76
SimonDaalas <sim.ler68@gmail.com>
2016-03-12 10:29:42 +0100

    Update Trading.py

--- Trading.py
+++ Trading.py
@@ -62,7 +62,7 @@ class Stock(Symbol):
 		date = self.datetime.datetime.now()
 
 		start=time.time()
-		content = self.urllib2.urlopen("http://real-chart.finance.yahoo.com/table.csv?s=%s&d=1&e=%d&f=%d&g=d&a=0&b=2&c=%d&ignore=.csv" %(self.name,date.month-1,date.day,date.year,self.start_year))
+		content = self.urllib2.urlopen("http://real-chart.finance.yahoo.com/table.csv?s=%s&d=1&e=%d&f=%d&g=%d&a=0&b=2&c=%d&ignore=.csv" %(self.name,date.month-1,date.day,date.year,self.start_year))
 		
 		#self.save(content.read())
 		self.Y=np.genfromtxt(self.StringIO(content.read()),delimiter=',',skip_header = 1)
